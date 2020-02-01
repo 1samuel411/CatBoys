bf57f62af3e14e7bbc1a1df21e143fae08c70095
SantiagoVega95 <57569117+SantiagoVega95@users.noreply.github.com>
2020-02-01 17:41:13 -0500

    Enemy AI
    I've created the scripts "EnemyController" and "Player Manager" in order to create a system where Enemies will chase the Player.

---
+++ CatBoyz/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    #region Singleton
+
+    public static PlayerManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    #endregion
+
+    public GameObject player;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
