22f8d119a965915c6cc5facd8fb4ff085889ca52
SantiagoVega95 <57569117+SantiagoVega95@users.noreply.github.com>
2020-02-02 12:34:53 -0500

    Santi's Health Update
    Finished a script called HealthSystem that manages health among both the player and enemies

---
+++ CatBoyz/Assets/Scripts/HealthSystem.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthSystem : MonoBehaviour
+{
+    public int maxHealth;
+    public int currentHealth;
+    //minimumHealth
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    
+    void Update()
+    {
+        if(currentHealth < 0)
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+
+    public void Damage(int damageAmount)
+    {
+        currentHealth = currentHealth - damageAmount;
+
+        //if currentHealth is less than minium
+        //current health equals 0;
+        //Destroy the object
+    }
+
+    public void SetMaxHealth()
+    {
+        currentHealth = maxHealth;
+    }
+}
