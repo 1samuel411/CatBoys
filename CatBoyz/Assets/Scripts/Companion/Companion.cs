67f3854d74895eb92a633a82cde4c82733567604
1samuel411 <armi.sam99@gmail.com>
2020-02-01 19:30:39 -0500

    Player Controller and companions

---
+++ CatBoyz/Assets/Scripts/Companion/Companion.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Companion : MonoBehaviour
+{
+
+    private new Rigidbody rigidbody;
+
+    public float attackRadius;
+    public float attackSpeed;
+    public float offset;
+
+    private bool hit = false;
+
+    private List<Transform> thingsToHit = new List<Transform>();
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        Vector2 v = rigidbody.velocity;
+        float angle = Mathf.Atan2(v.y, v.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        if(thingsToHit.Count > 0)
+        {
+            if((transform.position - (thingsToHit[0].position + (Vector3.forward * offset))).magnitude <= 0.5f)
+            {
+                thingsToHit.RemoveAt(0);
+            }
+            else
+            {
+                transform.position = Vector3.Lerp(transform.position, thingsToHit[0].position + (Vector3.forward * offset), attackSpeed * Time.deltaTime);
+            }
+        }
+        else
+        {
+            if (hit)
+                Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        thingsToHit = new List<Transform>();
+        rigidbody.isKinematic = true;
+        Collider[] hits = Physics.OverlapSphere(transform.position, attackRadius);
+        for (int i = 0; i < hits.Length; i++)
+        {
+            if (hits[i].transform.tag == "Enemy")
+            {
+                thingsToHit.Add(hits[i].transform);
+            }
+        }
+        hit = true;
+        thingsToHit = thingsToHit.OrderBy(x => Vector2.Distance(transform.position, x.transform.position)).ToList();
+    }
+}
---
+++ CatBoyz/Assets/Scripts/Companion/CompanionHandler.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompanionHandler : MonoBehaviour
+{
+
+    public GameObject companionAttackPrefab;
+
+    public float initAngle = 60;
+    public float xOffset;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 99999f))
+            {
+                SpawnCompanionAttack(hit.point);
+            }
+        }
+    }
+
+    void SpawnCompanionAttack(Vector3 targetPos)
+    {
+        Vector3 camPos = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, targetPos.z - Camera.main.transform.position.z));
+        Vector3 spawnPos = new Vector3(camPos.x - 5, Camera.main.transform.position.y - 5, targetPos.z);
+        float turretAngle = initAngle; //Turret is 10 deg tilted up (350 in inspector)
+        float g = Physics.gravity.magnitude; //G force
+        float y = (Camera.main.transform.position.y - 5) + targetPos.y; //Height of the target
+        float x = Vector3.Distance(targetPos, spawnPos); //distance to hit, slight off because turret is lifted up
+
+        Vector3 forward = Vector3.right;
+        float tanG = Mathf.Tan(turretAngle * Mathf.Deg2Rad);
+        float upper = Mathf.Sqrt(g) * Mathf.Sqrt(x) * Mathf.Sqrt(tanG * tanG + 1.0f);
+        float lower = Mathf.Sqrt(2 * tanG - ((2 * y) / x));
+
+        float velocity = upper / lower;
+
+        GameObject newObj = Instantiate(companionAttackPrefab);
+        newObj.transform.position = spawnPos;
+        newObj.transform.eulerAngles = new Vector3(0, 0,initAngle);
+        newObj.transform.localScale = Vector3.one;
+        newObj.transform.GetComponent<Rigidbody>().velocity = new Vector3(velocity * Mathf.Cos(turretAngle * Mathf.Deg2Rad), velocity * Mathf.Sin(turretAngle * Mathf.Deg2Rad));
+    }
+}
